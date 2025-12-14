c46a94324d25a22f43ba2473ba4aa3e555df5bd8
joaovirone <jovmamikl@gmail.com>
2025-12-14 10:57:48 -0300

    feat:criado arquivo dentro do dto para saques

---
+++ src/main/java/org/bank/fintech/dto/SaqueRequest.java
@@ -0,0 +1,16 @@
+package org.bank.fintech.dto;
+
+public class SaqueRequest {
+    
+
+    private Double valor;
+
+
+    public Double getValor (){
+        return valor;
+    }
+
+    public void SetValor (Double valor){
+        this.valor = valor;
+    }
+}
