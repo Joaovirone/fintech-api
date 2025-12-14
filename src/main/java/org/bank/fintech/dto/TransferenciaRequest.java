05d585bb9a2b1ce01083b1e7b900bdf337a3b480
joaovirone <jovmamikl@gmail.com>
2025-12-14 10:58:05 -0300

    feat:criado arquivo para transferencia dentro do dto

---
+++ src/main/java/org/bank/fintech/dto/TransferenciaRequest.java
@@ -0,0 +1,37 @@
+package org.bank.fintech.dto;
+
+public class TransferenciaRequest {
+    private Long idOrigem;
+    private Long idDestino;
+    private Double valor;
+
+
+    public Long getIdOrigem(){
+        return idOrigem;
+    }
+
+
+    public void setIdOrigem( Long idOrigem){
+        this.idOrigem = idOrigem;
+    }
+
+
+    public Long getIDestino(){
+        return idDestino;
+    }
+
+
+    public void setIdDestino( Long idDestino){
+        this.idDestino = idDestino;
+    }
+
+
+    public Double getValor(){
+        return valor;
+    }
+
+
+    public void setValor( Double valor){
+        this.valor = valor;
+    }
+}
