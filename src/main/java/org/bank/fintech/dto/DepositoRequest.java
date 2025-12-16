c0a3ccf9e5bdaa41555be357fdeeffc6cec470c7
joao <jovmamikl@gmail.com>
2025-12-16 11:24:15 -0300

    feat:Implementado validação @NotNull e @Positive, para o valor não ser nulo e ser positivo, respectivamente

--- src/main/java/org/bank/fintech/dto/DepositoRequest.java
+++ src/main/java/org/bank/fintech/dto/DepositoRequest.java
@@ -1,6 +1,11 @@
 package org.bank.fintech.dto;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public class DepositoRequest {
+
+    @NotNull(message="O valor é obrigatório.")
+    @Positive(message="O valor deve ser positivo")
     Double valor;
 
 
