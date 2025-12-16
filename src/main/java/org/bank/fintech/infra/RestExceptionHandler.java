f943e0c70eb9c51b0106d5771f169a38c7643fbf
joao <jovmamikl@gmail.com>
2025-12-16 11:27:08 -0300

    feat:Criado arquivo dentro da pasta Infra com o intuito de limpar o Controller mantendo o clean code.
    Basicamente esse RestExceptionHandler é um arquivo inde criamos as validações de erros ou de sucesso e deixamos guardadas aqui, assim as chamando posteriormente no controller

--- src/main/java/org/bank/fintech/infra/RestExceptionHandler.java
+++ src/main/java/org/bank/fintech/infra/RestExceptionHandler.java
@@ -1,8 +1,16 @@
 package org.bank.fintech.infra;
 
-import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.http.*;
 
 
 @ControllerAdvice
@@ -18,5 +26,19 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler{
     private ResponseEntity<String> tratarErroRuntime(RuntimeException exception){
         
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    public ResponseEntity<Map<String, String>> tratarErrosValidacao(MethodArgumentNotValidException ex){
+
+        Map<String, String> erros = new HashMap();
+
+
+        ex.getBindingResult().getAllErrors().forEach((error)-> {
+            String campo = ((FieldError) error).getField();
+            String mensagem = error.getDefaultMessage();
+            erros.put(campo, mensagem);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
     }
 }
