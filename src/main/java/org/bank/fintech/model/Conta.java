34e9f0e4a88fc756140f10161776d94777bb74c7
joao <jovmamikl@gmail.com>
2025-12-16 11:28:52 -0300

    feat: Implementado validação @NotBlank e @Pattern para o nome do titular e seu cpf serem obrigatórios e o cpf ter o formato padrão 000.000.000.00, respectivamente

--- src/main/java/org/bank/fintech/model/Conta.java
+++ src/main/java/org/bank/fintech/model/Conta.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 //Model: Representa a tabela do banco de dados no ambiente JAVA.
 //As classes aqui devem ser "burras". Ou seja, apenas dados (private attributes) e formas de acessá-los (Getters/Setters)
@@ -20,8 +22,13 @@ public class Conta{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private double saldo;
-    private String titular;
     private Date dataDeNascimento;
+
+    @NotBlank(message="O nome do titular é obrigatório.")
+    private String titular;
+
+    @NotBlank(message="O CPF é obrigatório.")
+    @Pattern(regexp="\\d{3}\\.\\d{3}\\.\\d{3}\\-\\{2}\\", message="O CPF deve estar no formato 000.000.000.00")
     private String cpf;
 
     @Column(nullable=false)
