3a1ddbd7d65fa13cb0cea4a06a9cdb020f922717
joao <jovmamikl@gmail.com>
2025-12-19 14:14:48 -0300

    feat: Feito a classe que vai gerar o Token de acesso

---
+++ src/main/java/org/bank/fintech/infra/security/TokenService.java
@@ -0,0 +1,44 @@
+package org.bank.fintech.infra.security;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.bank.fintech.model.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class TokenService {
+    
+
+    private String secret = "gostoso123";
+
+    public String gerarToken(Usuario usuario){
+
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.create().withIssuer("fintech-api").withSubject(usuario.getLogin()).withExpiresAt(gerarDataExpiracao()).sign(algorithm);
+        }catch(JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    public String validarToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm).build().verify(token).getSubject();
+        }catch (JWTVerificationException exception){
+            return "";
+        }
+    }
+    private Instant gerarDataExpiracao(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
