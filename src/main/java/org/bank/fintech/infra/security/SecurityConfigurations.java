6ba1b0fe16d744ce9c87ebc81043a34e6f0ac330
joaovirone <jovmamikl@gmail.com>
2025-12-20 19:57:44 -0300

    refactor:adicionado todos os parametros para configuração de segurança e autenticação

--- src/main/java/org/bank/fintech/infra/security/SecurityConfigurations.java
+++ src/main/java/org/bank/fintech/infra/security/SecurityConfigurations.java
@@ -1,5 +1,67 @@
 package org.bank.fintech.infra.security;
 
+import org.bank.fintech.service.AuthorizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.password.*;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfigurations {
     
+    @Autowired
+    private SecurityFilter securityFilter;
+
+    @Autowired
+    private AuthorizationService authorizationService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity.csrf(csrf -> csrf.disable()).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize-> authorize
+            //.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+            //.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+            .requestMatchers("/auth/**").permitAll()
+            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+
+            .requestMatchers(HttpMethod.DELETE, "/contas/**").hasRole("admin")
+
+            .anyRequest().authenticated()
+        )
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class).build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(authorizationService);
+
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+
+        return authenticationConfiguration.getAuthenticationManager();
+    }   
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
 }
