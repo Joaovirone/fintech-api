4d55a4e997e27e84c18a297136a8bb11f87a71b0
joaovirone <jovmamikl@gmail.com>
2025-12-22 20:28:44 -0300

    refactor: ajustada nome de variável dataDeNascimento para dataNascimento

--- src/main/java/org/bank/fintech/controller/ContaController.java
+++ src/main/java/org/bank/fintech/controller/ContaController.java
@@ -52,7 +52,7 @@ public class ContaController {
             Conta novaConta = new Conta();
             novaConta.setTitular(dados.getTitular());
             novaConta.setCpf(dados.getCpf());
-            novaConta.setDataDeNascimento(dados.getDataDeNascimento());
+            novaConta.setDataNascimento(dados.getDataNascimento());
 
             Conta contaCriada = service.criar(novaConta,dados.getValorInicial());
 
