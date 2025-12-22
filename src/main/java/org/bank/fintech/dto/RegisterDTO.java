aeaba749d5e19990721f3270df839488417bdf08
joaovirone <jovmamikl@gmail.com>
2025-12-22 20:03:34 -0300

    refactor: ajustada a classe para record

--- src/main/java/org/bank/fintech/dto/RegisterDTO.java
+++ src/main/java/org/bank/fintech/dto/RegisterDTO.java
@@ -1,5 +1,15 @@
 package org.bank.fintech.dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public record RegisterDTO(String login, String senha, String role) {
-    
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class RegisterDTO {
+    String login;
+    String senha;
+    String role;
 }
